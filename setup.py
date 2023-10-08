ec63c3ca9dd894d6c4b3f3a61a56cacb7b848920
Julius Gonsior <julius@gaenseri.ch>
2023-10-08 19:31:46 +0200

    deactivated forever cython stuff

--- setup.py
+++ setup.py
@@ -20,67 +20,8 @@ if on_rtd:
     install_requires = []
     tests_require = []
 else:
-    # from Cython.Build import cythonize
-    # from Cython.Distutils import build_ext
-    import numpy
-    import numpy.distutils
-
-    if sys.platform == "darwin":
-        print("Platform Detection: Mac OS X. Link to openblas...")
-        extra_link_args = []
-        libraries = ["openblas"]
-        library_dirs = [
-            "/opt/local/lib",
-            "/usr/local/opt/openblas/lib",  # for brew installs
-        ]
-        include_dirs = numpy.distutils.misc_util.get_numpy_include_dirs() + [
-            "/opt/local/include",
-            "/usr/local/opt/openblas/include",
-        ]  # for brew installs
-    else:
-        # assume linux otherwise, unless we support Windows in the future...
-        print("Platform Detection: Linux. Link to liblapacke...")
-        extra_link_args = []
-        include_dirs = numpy.distutils.misc_util.get_numpy_include_dirs() + [
-            "/usr/include/"
-        ]
-        libraries = ["lapacke", "lapack", "blas"]
-        library_dirs = ["/usr/lib"]
-
     extensions = []
-    if BUILD_VARIANCE_REDUCTION:
-        print("Build VarianceReduction...")
-        extensions.append(
-            Extension(
-                "libact.query_strategies._variance_reduction",
-                ["libact/query_strategies/src/variance_reduction/variance_reduction.c"],
-                extra_link_args=extra_link_args,
-                extra_compile_args=["-std=c11"],
-                include_dirs=include_dirs,
-                libraries=libraries,
-                library_dirs=library_dirs,
-            )
-        )
-    if BUILD_HINTSVM:
-        print("Build HintSVM...")
-        extensions.append(
-            Extension(
-                "libact.query_strategies._hintsvm",
-                sources=[
-                    "libact/query_strategies/_hintsvm.pyx",
-                    "libact/query_strategies/src/hintsvm/libsvm_helper.c",
-                    "libact/query_strategies/src/hintsvm/svm.cpp",
-                ],
-                include_dirs=[
-                    numpy.get_include(),
-                    "libact/query_strategies/src/hintsvm/",
-                ],
-                extra_compile_args=["-lstdc++"],
-            )
-        )
-
-    extensions = cythonize(extensions)
-    cmdclasses = {"build_ext": build_ext}
+    cmdclasses = {}
     setup_requires = []
     with open("./requirements.txt") as f:
         requirements = f.read().splitlines()
@@ -92,7 +33,7 @@ else:
 
 setup(
     name="libact",
-    version="0.1.6.2",
+    version="0.1.6.3",
     description="Pool-based active learning in Python",
     long_description=open("README.md", "r", newline="", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
